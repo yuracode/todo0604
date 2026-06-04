62f65d44a611bd543b879ed436532a209ad62f13
Hiroyuki Sasaki <hry-sasaki@yoshida-g.ac.jp>
2026-06-04 11:20:58 +0900

    Click追加

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ import { useState } from 'react'
 import Greeting from './Greeting.jsx'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
+  let cnt = 0
+  function countup(){
+    setCount(count+1)
+    console.log("押された", count);
+  }
 
   return (
     <>
       <h1>見出しを書く</h1>
-      <Greeting name="cyber" greet="こんばんは" />
+      <Greeting name="cyber" onclick={countup} greet="こんばんは" />
+      {count}
+      <button >押して</button>
     </>
   )
 }
--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,7 +1,8 @@
 import "tailwindcss"
 
 function Greeting(props){
-    const {name,greet} = props
-    return <p className="font-bold text-blue-500">{name}さん。{greet}</p>
+    console.log("Greeting")
+    const {name,greet,onclick} = props
+    return <p onClick={()=>onclick()}className="font-bold text-blue-500">{name}さん。{greet}</p>
 }
 export default Greeting
